c37f7ca6309da9eddb73cd4a6fa72c8bbb21e4b6
Pradeep-Madhavaraj <pradeepm6059@gmail.com>
2024-06-28 12:21:08 +0530

    configured varibales

--- Project/__init__.py
+++ Project/__init__.py
@@ -22,8 +22,8 @@ login_manager.login_message_category = 'info'
 
 mail = Mail(app)
 app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
-app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
-app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS')
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
