bd63bf035a1b14bea49c298030e7387683b1559f
Pradeep-Madhavaraj <pradeepm6059@gmail.com>
2024-06-28 15:37:39 +0530

    Updated environment variables

--- Project/__init__.py
+++ Project/__init__.py
@@ -3,29 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
-from dotenv import load_dotenv
 import os
 
 
-load_dotenv()
-
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config["SECURITY_PASSWORD_SALT"] = os.environ.get('SECURITY_PASSWORD_SALT')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config["SECURITY_PASSWORD_SALT"] = os.getenv('SECURITY_PASSWORD_SALT')
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 mail = Mail(app)
-app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 
 from Project.main.routes import main
