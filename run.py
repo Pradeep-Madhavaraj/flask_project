d3579a4cf7471d1c4c7de9d08378dc575da13a56
Pradeep-Madhavaraj <pradeepm6059@gmail.com>
2024-06-28 11:34:48 +0530

    set debug to false

--- run.py
+++ run.py
@@ -2,8 +2,5 @@ from App import app, db
 
 
 if __name__ == '__main__':
-    # with app.app_context():
-    #     db.create_all()
-    #     db.session.commit()
-    app.run(debug=True)
+    app.run()
 
