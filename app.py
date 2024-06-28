9439428882f84db22fae8b02af5f5c548194cd25
Pradeep-Madhavaraj <pradeepm6059@gmail.com>
2024-06-28 13:41:45 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from Project import app, db
-import os
+from Project import app
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
